2e59972d31ca08fd4145b72087a16439f0087ca3
KAJ7 <zekewotring@gmail.com>
2020-07-13 21:01:34 -0800

    testing

--- Cogs/Class.py
+++ Cogs/Class.py
@@ -11,21 +11,38 @@ class Class(commands.Cog):
     def setup(bot):
         bot.add_cog(Profile(bot))
 
-    @commands.group(aliases=['class','c'])
+    @commands.command(aliases=['class','c'])
     async def dash(self, ctx):
-        if ctx.invoked_subcommand is None:
-            account, first_time = utils.get_profile(ctx.author.id)
-            embed = discord.Embed(title="<:inv:732103029213364295> Your Classes",
-                                  color = config.MAINCOLOR)
-            for aclass in config.CLASSES.find({'members': ctx.author.id}):
-                classname = aclass['name']
-                classcode = aclass['code']
-                classowner = aclass['owner']
-                embed.add_field(name="<:enter:732105777577459723> " + classname + " ["+classcode+"]",
-                                value="Teacher: <@" + str(classowner) + ">\nSize: " + str(len(aclass['members'])) + "\n",
-                                inline=False)
-            embed.description = "*Use `d!join` to join and `d!create` to create a class.*"
-            await ctx.send(embed=embed)
+        account, first_time = utils.get_profile(ctx.author.id)
+        embed = discord.Embed(title="<:inv:732103029213364295> Your Classes",
+                              color=config.MAINCOLOR)
+        for aclass in config.CLASSES.find({'members': ctx.author.id}):
+            classname = aclass['name']
+            classcode = aclass['code']
+            classowner = aclass['owner']
+            embed.add_field(name="<:enter:732105777577459723> " + classname + " [" + classcode + "]",
+                            value="Teacher: <@" + str(classowner) + ">\nSize: " + str(len(aclass['members'])) + "\n",
+                            inline=False)
+        embed.description = "*Use `d!join` to join and `d!create` to create a class.*"
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def join(self, ctx, code:str=None):
+        account, first_time = utils.get_profile(ctx.author.id)
+        chosen_class = config.CLASSES.find_one({'code': code})
+        if chosen_class is not None:
+            config.CLASSES.update_one({'code': code}, {'$push': {'members': ctx.author.id}})
+            embed=discord.Embed(title="<:plus:732103029435924491> Class Joined", description=f"You have enrolled in **{chosen_class['name']}**.\nYou can see information about the class by typing `d!class {chosen_class['code']}`", color=config.MAINCOLOR)
+            await ctx.send(embed)
+            teacher_account, first_time = utils.get_profile(chosen_class['owner'])
+            teacher = self.bot.get_user(chosen_class['owner'])
+            if teacher is not None and teacher_account['teacher_notifications']:
+                embed=discord.Embed(title="<:pin:732461353830449173> Class Notification", description=f"A student named {ctx.author.name} ({str(ctx.author.id)}) has enrolled in {chosen_class['name']} [{chosen_class['code']}]", color=config.MAINCOLOR)
+                embed.set_footer(text="to disable notifications type 'd!noti disable'", icon_url="https://cdn.discordapp.com/emojis/732116410553073674.png?v=1")
+                await teacher_account.send(embed=embed)
+
+
+
 
 def setup(bot):
     bot.add_cog(Class(bot))
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import config
 def get_profile(user_id):
     document = config.USERS.find_one({'user_id': user_id})
     if document is None:
-        document = {'user_id': user_id, 'is_student': True, 'google_classroom': None, 'bio': None, 'classes': []}
+        document = {'user_id': user_id, 'is_student': True, 'google_classroom': None, 'bio': None, 'classes': [], 'teacher_notifications': True, 'student_notifications': True}
         config.USERS.insert_one(document)
         return document, True
     return document, False
