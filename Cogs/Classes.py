c4d9030c0f9c7b281d7ed924b45565f20933bb66
Luis Vervaet <luisrvervaet@gmail.com>
2020-07-14 17:18:56 -0400

    Update Classes.py

--- Cogs/Classes.py
+++ Cogs/Classes.py
@@ -40,6 +40,31 @@ class Classes(commands.Cog):
                 embed=discord.Embed(title="<a:bell:732103030488432720> Class Notification", description=f"A student named {ctx.author.name} ({str(ctx.author.id)}) has enrolled in {chosen_class['name']} [{chosen_class['code']}]", color=config.MAINCOLOR)
                 embed.set_footer(text="to disable notifications type 'd!noti disable'", icon_url="https://cdn.discordapp.com/emojis/732116410553073674.png?v=1")
                 await teacher.send(embed=embed)
+        else:
+            embed = discord.Embed(title="<a:cross:732103029712617482> Class Join Error", description=f"Sorry, This class does not exist, please try again.", color=config.ERRORCOLOR)
+            await ctx.send(embed=embed)
+    @commands.command()
+    async def leave(self, ctx, code:str=None):
+        account, first_time = utils.get_profile(ctx.author.id)
+        chosen_class = config.CLASSES.find_one({'code': code})
+        if chosen_class is not None:
+            if ctx.author.id in chosen_class['members']:
+                config.CLASSES.update_one({'code': code}, {'$pull': {'members': ctx.author.id}})
+                embed = discord.Embed(title="<:minus:732103028726824982> Left Class", description=f"You have left **{chosen_class['name']}**.", color=config.MAINCOLOR)
+                await ctx.send(embed=embed)
+                teacher_account, first_time = utils.get_profile(chosen_class['owner'])
+                teacher = self.bot.get_user(chosen_class['owner'])
+                if teacher is not None and teacher_account['teacher_notifications']:
+                    embed=discord.Embed(title="<a:bell:732103030488432720> Class Notification", description=f"A Student named {ctx.author.name} ({str(ctx.author.id)}) has enrolled in {chosen_class['name']} [{chosen_class['code']}]", color=config.MAINCOLOR)
+                    embed.set_footer(text="to disable notifications type 'd!noti disable'", icon_url="https://cdn.discordapp.com/emojis/732116410553073674.png?v=1")
+                    await teacher.send(embed=embed)
+            else:
+                embed = discord.Embed(title="<a:cross:732103029712617482> Class Leave Error", description=f"Sorry, You are not in this Class, please try again.", color=config.ERRORCOLOR)
+                await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="<a:cross:732103029712617482> Class Leave Error", description=f"Sorry, You are not in this Class, please try again.", color=config.ERRORCOLOR)
+            await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Classes(bot))
