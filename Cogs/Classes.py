994d2d2939f4a26289b60030927cbcec4ecfc5f6
Luis Vervaet <luisrvervaet@gmail.com>
2020-07-14 23:00:53 -0400

    KAJ WTF

--- Cogs/Classes.py
+++ Cogs/Classes.py
@@ -67,7 +67,8 @@ class Classes(commands.Cog):
                     embed.add_field(name="<:people:732103029565947934> Class Directory", value=mystring)
 
                     if ctx.author.id == the_class['owner']:
-                        await ctx.send(embed=embed)
+                        pass
+                    await ctx.send(embed=embed)
                 else:
                     embed = discord.Embed(title="<:cross:732103029712617482> That class does not exist",
                                           color=config.MAINCOLOR)
